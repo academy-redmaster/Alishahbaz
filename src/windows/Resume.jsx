9bf6a17c2f5fd47e3b6ad868705efb731936b0cc
ali <frontend.bootcamp.ai@gmail.com>
2026-06-22 23:05:21 +0330

    fix: resolve PDF resume display issue

--- src/windows/Resume.jsx
+++ src/windows/Resume.jsx
@@ -6,13 +6,13 @@ import { pdfjs, Document, Page } from "react-pdf";
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  "pdfjs-dist/build/pdf.worker.min.mjs",
-  import.meta.url,
-).toString();
+// pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+//   "pdfjs-dist/build/pdf.worker.min.mjs",
+//   import.meta.url,
+// ).toString();
 
 // ========= for productions
-// pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf/pdf.worker.min.mjs`;
+pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf/pdf.worker.min.mjs`;
 
 const Resume = () => {
   return (
